9a50556bb05348f1c889cde6ca3fb8a60c8d33de
agilz <agil76317@gmail.com>
2026-01-21 15:29:23 +0700

    Update mysql2 dependency to version 3.16.1 and refactor database connection configuration to use environment variables

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,12 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'perpustakaan_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
